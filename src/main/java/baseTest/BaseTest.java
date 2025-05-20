b3582a08c9aa33af2a4d1dd9c849975f05bd85ed
Irreffadilla <55435950+ffadilla@users.noreply.github.com>
2025-05-20 23:22:54 +0700

    fixing some unexpected handle

--- src/main/java/baseTest/BaseTest.java
+++ src/main/java/baseTest/BaseTest.java
@@ -1,7 +1,11 @@
 package baseTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -12,6 +16,15 @@ public class BaseTest {
     //Preparing env
     @BeforeMethod
     public void setUp() {
+
+        // ignore/disable windows dialog during the test
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        options.setExperimentalOption("prefs", prefs);
+        options.addArguments("--disable-save-password-bubble");
+
         String projectPath = System.getProperty("user.dir");
         System.setProperty("webdriver.chrome.driver", projectPath + "/src/test/resources/drivers/chromedriver.exe");
         driver = new ChromeDriver();
